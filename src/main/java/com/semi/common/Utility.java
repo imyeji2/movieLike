5924aa2c8ddafe31100cde4949dbad6d94884b88
imdam2 <137968566+imdam2@users.noreply.github.com>
2023-07-14 15:11:39 +0900

    영화 추가

--- src/main/java/com/semi/common/Utility.java
+++ src/main/java/com/semi/common/Utility.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class Utility {
 	//자료실 업로드 관련 상수
 	public static final String UP_PATH="/upload";
-	public static final String ACTOR_PATH="C:\\Users\\yj\\git\\SemiProject\\src\\main\\webapp\\images\\movie\\actor";
-	public static final String DIRECTOR_PATH="C:\\Users\\yj\\git\\SemiProject\\src\\main\\webapp\\images\\movie\\director";
-	public static final String MOVIE_PATH="C:\\Users\\yj\\git\\SemiProject\\src\\main\\webapp\\images\\movie\\content";
-		
+	public static final String ACTOR_PATH="D:\\lecture\\workspace_list\\semi_ws\\SemiProject\\src\\main\\webapp\\images\\movie\\actor";
+	public static final String DIRECTOR_PATH="D:\\lecture\\workspace_list\\semi_ws\\SemiProject\\src\\main\\webapp\\images\\movie\\director";
+	public static final String MOVIE_PATH="D:\\lecture\\workspace_list\\semi_ws\\SemiProject\\src\\main\\webapp\\images\\movie\\content";
+	
 	public static String displayRe(int step) {
 		String result="";
 
